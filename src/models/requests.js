46ebda56022037219b388da536730c82fc9c3265
Layyyth <laithsocials@gmail.com>
2024-10-14 14:29:15 +0300

    GET req instead of post

--- src/models/requests.js
+++ src/models/requests.js
@@ -4,16 +4,21 @@ const url = "https://flaskmicro-aeeb2f2bef86.herokuapp.com/";
 
 async function predictCalories(id) {
   try {
-    // Send a GET request with user_id as a query parameter
+    // Send a GET request with user_id as a query parameter and custom headers
     const res = await axios.get(url + "predict", {
       params: {
         user_id: id,
       },
+      headers: {
+        'Content-Type': 'application/json',  // Ensures the request is JSON format
+        'Accept': 'application/json',        // Informs server to respond with JSON
+        'Access-Control-Allow-Origin': '*',  // Allows CORS, but this is typically set by the server
+      }
     });
-    console.log(res);
-    return res;
+    console.log(res.data);
+    return res.data;
   } catch (err) {
-    console.log(err);
+    console.error("Error fetching calorie prediction:", err.message);
   }
 }
 
