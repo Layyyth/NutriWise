1abf0d4ddcaaf8583e924a8a7376d628d1a1bf18
Ayman Bazyan <103147442+aymanbazyan@users.noreply.github.com>
2024-10-16 11:01:00 +0300

    trash

--- src/models/requests.js
+++ src/models/requests.js
@@ -5,16 +5,17 @@ const url = "https://flaskmicro-aeeb2f2bef86.herokuapp.com/";
 async function predictCalories(id) {
   try {
     // Send a GET request with user_id as a query parameter and custom headers
-    const res = await axios.get(url + "predict", {
+    const res = await axios.post(url + "predict", {
       params: {
         user_id: id,
       },
       headers: {
-        'Content-Type': 'application/json',  // Ensures the request is JSON format
-        'Accept': 'application/json',        // Informs server to respond with JSON
-        'Access-Control-Allow-Origin': '*',  // Allows CORS, but this is typically set by the server
-      }
+        "Content-Type": "application/json", // Ensures the request is JSON format
+        Accept: "application/json", // Informs server to respond with JSON
+        "Access-Control-Allow-Origin": "*", // Allows CORS, but this is typically set by the server
+      },
     });
+
     console.log(res.data);
     return res.data;
   } catch (err) {
